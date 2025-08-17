27f1e9bd6fd24270aef713abc0669059bf424b1c
Eshunned <esinha543@gmail.com>
2025-08-18 02:29:31 +0530

    some changes

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,6 +1,6 @@
 // app/api/test/route.js
 import { NextResponse } from "next/server";
-import { prisma } from "../../lib/prisma"; // if you don't have @ alias, use ../../lib/prisma
+import { prisma } from "../../../lib/prisma"; // if you don't have @ alias, use ../../lib/prisma
 
 export async function GET() {
   try {
